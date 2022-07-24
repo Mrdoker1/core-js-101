bdf17bbd83f6f07dd42d0bb4ac08c8d77e49d5a2
Viachaslau Kulbitski <fixrapdok@gmail.com>
2022-07-24 15:32:04 +0300

    feat: resolve task

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -27,8 +27,23 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+  const three = num;
+  const five = num;
+
+  if (Number.isInteger(three / 3) && Number.isInteger(five / 5)) {
+    return 'FizzBuzz';
+  }
+
+  if (Number.isInteger(five / 5)) {
+    return 'Buzz';
+  }
+
+  if (Number.isInteger(three / 3)) {
+    return 'Fizz';
+  }
+
+  return num;
 }
 
 
@@ -43,8 +58,10 @@ function getFizzBuzz(/* num */) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  if (n < 0) return -1;
+  if (n === 0) return 1;
+  return (n * getFactorial(n - 1));
 }
 
 
@@ -60,8 +77,13 @@ function getFactorial(/* n */) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  let result = 0;
+
+  for (let i = n1; i <= n2; i += 1) {
+    result += i;
+  }
+  return result;
 }
 
 
@@ -80,8 +102,8 @@ function getSumBetweenNumbers(/* n1, n2 */) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  return (a + c > b) && (a + b > c) && (b + c > a);
 }
 
 
@@ -117,8 +139,21 @@ function isTriangle(/* a, b, c */) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const rect1Width = rect1.left + rect1.width;
+  const rect2Width = rect2.left + rect2.width;
+  const rect1Height = rect1.top + rect1.height;
+  const rect2Height = rect2.top + rect2.height;
+
+  if (rect1.top > rect2Height || rect2.top > rect1Height) {
+    return false;
+  }
+
+  if (rect1.left > rect2Width || rect2.left > rect1Width) {
+    return false;
+  }
+
+  return true;
 }
 
 
@@ -148,8 +183,10 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const distanceX = (point.x - circle.center.x) * (point.x - circle.center.x);
+  const distanceY = (point.y - circle.center.y) * (point.y - circle.center.y);
+  return distanceX + distanceY < circle.radius * circle.radius;
 }
 
 
@@ -164,8 +201,13 @@ function isInsideCircle(/* circle, point */) {
  *   'abracadabra'  => 'c'
  *   'entente' => null
  */
-function findFirstSingleChar(/* str */) {
-  throw new Error('Not implemented');
+function findFirstSingleChar(str) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str.lastIndexOf(str[i]) === str.indexOf(str[i])) {
+      return str[i];
+    }
+  }
+  return null;
 }
 
 
@@ -191,8 +233,14 @@ function findFirstSingleChar(/* str */) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+  let start = '[';
+  let end = ']';
+  if (!isStartIncluded) start = '(';
+  if (!isEndIncluded) end = ')';
+  return `${start}${min}, ${max}${end}`;
 }
 
 
@@ -208,8 +256,8 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 
@@ -225,8 +273,8 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  return parseInt(`${num}`.split('').reverse().join(''), 10);
 }
 
 
@@ -250,8 +298,25 @@ function reverseInteger(/* num */) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  const arr = ccn.toString().split('');
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i += 1) {
+    let num = parseInt(arr[i], 10);
+
+    if ((arr.length - i) % 2 === 0) {
+      num *= 2;
+
+      if (num > 9) {
+        num -= 9;
+      }
+    }
+
+    sum += num;
+  }
+
+  return sum % 10 === 0;
 }
 
 /**
@@ -268,8 +333,10 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  if (num < 10) return num;
+  const sum = `${num}`.split('').reduce((a, value) => parseInt(value, 10) + a, 0);
+  return getDigitalRoot(sum);
 }
 
 
@@ -319,8 +386,8 @@ function isBracketsBalanced(/* str */) {
  *    365, 4  => '11231'
  *    365, 10 => '365'
  */
-function toNaryString(/* num, n */) {
-  throw new Error('Not implemented');
+function toNaryString(num, n) {
+  return num.toString(n);
 }
 
 
@@ -359,8 +426,29 @@ function getCommonDirectoryPath(/* pathes */) {
  *                         [ 6 ]]
  *
  */
-function getMatrixProduct(/* m1, m2 */) {
-  throw new Error('Not implemented');
+function getMatrixProduct(m1, m2) {
+  const rowsM1 = m1.length;
+  const colsM1 = m1[0].length;
+  const rowsM2 = m2.length;
+  const colsM2 = m2[0].length;
+  const result = [];
+
+
+  if (colsM1 !== rowsM2) { return false; }
+
+  for (let i = 0; i < rowsM1; i += 1) { result[i] = []; }
+
+  for (let j = 0; j < colsM2; j += 1) {
+    for (let k = 0; k < rowsM1; k += 1) {
+      let temp = 0;
+      for (let q = 0; q < rowsM2; q += 1) {
+        temp += m1[k][q] * m2[q][j];
+        result[k][j] = temp;
+      }
+    }
+  }
+
+  return result;
 }
 
 
